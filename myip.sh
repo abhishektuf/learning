2a8546ccc6fbc8b44e6bfabc4b7ef2302bdf9736
Abhishek <5516977+abhishektuf@users.noreply.github.com>
2021-06-19 08:53:38 +0530

    Update myip.sh

--- myip.sh
+++ myip.sh
@@ -1,11 +1,20 @@
-echo "icanhazip"
-#echo "^---ipv4"
-#curl -4 icanhazip.com
-#echo "^---ipv6"
-curl -6 icanhazip.com
-echo "~~~~~~~~~~~~~~~"
-echo "cisco"
-host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}'
-echo "~~~~~~~~~~~~~~~"
-echo "google"
-dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'
+#!/bin/sh
+# Author : Abhishek Kumar
+# No Copyright
+# Script follows here:
+
+#Getting IP from icanhazip.com
+ip1=`curl -s -6 icanhazip.com`
+#Getting IP from Cisco opendns
+ip2=`host myip.opendns.com resolver1.opendns.com | grep "myip.opendns.com has" | awk '{print $4}'`
+#Getting IP from Google DNS
+ip3=`dig TXT +short o-o.myaddr.l.google.com @ns1.google.com | awk -F'"' '{ print $2}'`
+#Comparing the IPs from diff providers
+if [[ $ip1 == $ip2 ]]; then
+  if [[ $ip2 == $ip3 ]]; then
+    # Output the IP on screen
+    echo "Your Public IP is :" $ip3
+  fi
+fi
+#Trying to find the owner/provider of the IP
+whois $ip3 |grep descr |uniq
